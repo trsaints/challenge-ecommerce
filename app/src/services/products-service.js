0c9a30a093917e3e0fcaf809c41152d4de542ed8
trsaints <thiago.dev2k3@gmail.com>
2022-09-03 12:55:28 -0300

    Listagem de produtos separada por limitador

--- app/src/services/products-service.js
+++ app/src/services/products-service.js
@@ -1,18 +1,29 @@
-async function getProducts(path) {
+async function getProducts(path, type) {
   const binURL = `https://api.jsonbin.io/v3/b/630a3993a1610e6386119477/`;
   const apiKey = "$2b$10$IZVLAix31GSpjITpSAO8ketbp/8/A2MXyuqhVejENVI9aJxOvnvRy";
 
   const options = {
-    method: "GET",
-    headers: {
-      "X-Access-Key": `${apiKey}`,
-      "X-Bin-Meta": false,
-      "X-JSON-Path": `$..${path}[:6]`,
-      "Content-Type": "application/json",
+    abridged: {
+      method: "GET",
+      headers: {
+        "X-Access-Key": `${apiKey}`,
+        "X-Bin-Meta": false,
+        "X-JSON-Path": `$..${path}[:6]`,
+        "Content-Type": "application/json",
+      },
+    },
+    full: {
+      method: "GET",
+      headers: {
+        "X-Access-Key": `${apiKey}`,
+        "X-Bin-Meta": false,
+        "X-JSON-Path": `$..${path}`,
+        "Content-Type": "application/json",
+      },
     },
   };
 
-  const response = await fetch(binURL, options);
+  const response = await fetch(binURL, options[type]);
 
   if (response.ok) {
     return response.json();
@@ -22,5 +33,5 @@ async function getProducts(path) {
 }
 
 export const productsService = {
-  getProducts
-}
+  getProducts,
+};
