2d539ee457d2d70141d3c61d0096e8ce2cb3c324
trsaints <thiago.dev2k3@gmail.com>
2022-09-03 13:03:24 -0300

    Listagem de produtos separada por limitador

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,8 @@
 import { productController } from "./controllers/products-controller.js";
 
 (() => {
+  const catalog = document.querySelector("[data-catalog]");
   document.addEventListener("DOMContentLoaded", () => {
-    productController.renderProductsLists();
+    productController.renderProductsLists(catalog.dataset.catalog);
   });
 })();
