d20965cb90dbc011b03fdb8f45f47b88c58870f4
trsaints <thiago.dev2k3@gmail.com>
2022-09-09 09:32:18 -0300

    Método assíncrono reorganizado

--- app/src/services/products-service.js
+++ app/src/services/products-service.js
@@ -1,37 +1,30 @@
-async function getProducts(path, type) {
-  const binURL = `https://api.jsonbin.io/v3/b/630a3993a1610e6386119477/`;
-  const apiKey = "$2b$10$IZVLAix31GSpjITpSAO8ketbp/8/A2MXyuqhVejENVI9aJxOvnvRy";
-
-  const options = {
-    abridged: {
-      method: "GET",
-      headers: {
-        "X-Access-Key": `${apiKey}`,
-        "X-Bin-Meta": false,
-        "X-JSON-Path": `$..${path}[:6]`,
-        "Content-Type": "application/json",
-      },
-    },
-    full: {
-      method: "GET",
-      headers: {
-        "X-Access-Key": `${apiKey}`,
-        "X-Bin-Meta": false,
-        "X-JSON-Path": `$..${path}.*`,
-        "Content-Type": "application/json",
-      },
-    },
-  };
-
-  const response = await fetch(binURL, options[type]);
-
-  if (response.ok) {
-    return response.json();
-  }
-
-  throw new Error("Não foi possível listar os produtos.");
-}
-
 export const productsService = {
-  getProducts,
+  async getProducts(category, context) {
+    const binURL = `https://api.jsonbin.io/v3/b/630a3993a1610e6386119477/`;
+    const apiKey =
+      "$2b$10$IZVLAix31GSpjITpSAO8ketbp/8/A2MXyuqhVejENVI9aJxOvnvRy";
+
+    const contextPath = {
+      abridged: `$..${category}[:6]`,
+      full: `$..${category}.*`,
+    };
+
+    const option = {
+      method: "GET",
+      headers: {
+        "X-Access-Key": `${apiKey}`,
+        "X-Bin-Meta": false,
+        "X-JSON-Path": `${contextPath[context]}`,
+        "Content-Type": "application/json",
+      },
+    };
+
+    const response = await fetch(binURL, option);
+
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error("Não foi possível listar os produtos.");
+  }
 };
