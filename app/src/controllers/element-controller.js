0771c2cfdc5d1159a02b6da2c34f172419c5b4bc
trsaints <thiago.dev2k3@gmail.com>
2022-09-09 09:30:15 -0300

    Função (estética) para lidar com elementos criada

--- app/src/controllers/element-controller.js
+++ app/src/controllers/element-controller.js
@@ -1,4 +1,13 @@
 export const elementController = {
+  load(target) {
+    if (target.classList.contains("loading")) {
+      target.classList.remove("loading");
+      return;
+    }
+
+    throw new Error("Não foi possível carregar elemento: inexistente ou já está carregado")
+  },
+
   generate(element, selector) {
     const currElement = document.createElement(element);
     currElement.classList.add(selector);
@@ -16,5 +25,5 @@ export const elementController = {
     const currContext = document.querySelector("[data-catalog]");
 
     return currContext.dataset.catalog === context;
-  }
+  },
 };
