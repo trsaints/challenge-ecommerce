13bb4e5af07458b6ffca76c5adad84916d882f9a
trsaints <thiago.dev2k3@gmail.com>
2022-09-10 16:47:14 -0300

    Novos métodos para manipular elementos criados

--- app/src/controllers/element-controller.js
+++ app/src/controllers/element-controller.js
@@ -1,11 +1,20 @@
 export const elementController = {
+  show(target) {
+    if (target.classList.contains("hidden")) {
+      target.classList.remove("hidden");
+    }
+  },
+
+  hide(target) {
+    if (!target.classList.contains("hidden")) {
+      target.classList.add("hidden");
+    }
+  },
+
   load(target) {
     if (target.classList.contains("loading")) {
       target.classList.remove("loading");
-      return;
     }
-
-    throw new Error("Não foi possível carregar elemento: inexistente ou já está carregado")
   },
 
   generate(element, selector) {
@@ -25,5 +34,9 @@ export const elementController = {
     const currContext = document.querySelector("[data-catalog]");
 
     return currContext.dataset.catalog === context;
+  },
+
+  getData(target, data) {
+    return target.getAttribute(`data-${data}`) !== null;
   },
 };
